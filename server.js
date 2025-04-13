b5566bf0234d9df30090834720f30ffbaa3b1e25
Felipe Min <felipemin03@gmail.com>
2025-04-13 18:40:22 -0400

    established db connection

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const url =
+  "mongodb+srv://tim56rus:ZQzyfMmhOOUfHrhP@cluster0.x3n5qfd.mongodb.net/TravelGuide?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(url);
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+  }
+}
+connectDB();
+
+// Start the Express server on port 5001
+app.listen(5001, () => {
+  console.log("Server running on port 5001");
+});
+
+// Set up CORS headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
