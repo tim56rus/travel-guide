b90cd6673b0bfeb417f7bc8874a85c1ed6f8aa3c
Felipe Min <felipemin03@gmail.com>
2025-04-13 23:28:36 -0400

    proxy setting added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-
-	server: {
-		port: 3000,
-	},
-
-
-	plugins: [react()],
-})
+  server: {
+    port: 3000,
+    proxy: {
+      // this proxy forwards any requests starting with /api to the backend server running on port 5000
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+  plugins: [react()],
+});
